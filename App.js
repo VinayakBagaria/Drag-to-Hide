3543d1b85aebb60a5d2716017eb79a5268a2e226
VinayakBagaria <workvinayak95@gmail.com>
2018-08-25 15:12:54 +0530

    main container area design

--- App.js
+++ App.js
@@ -6,15 +6,52 @@
  * @flow
  */
 
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
 import Draggable from './components/Draggable';
-
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <Draggable />
+      <View style={styles.mainContainer}>
+        <View style={styles.dropZone}>
+          <Text style={styles.text}>Drop them here!</Text>
+        </View>
+        <View style={styles.ballContainer} />
+        <View style={styles.row}>
+          <Draggable />
+          <Draggable />
+          <Draggable />
+          <Draggable />
+          <Draggable />
+        </View>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+  },
+  ballContainer: {
+    height: 200,
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  dropZone: {
+    height: 200,
+    backgroundColor: '#00334d',
+  },
+  text: {
+    marginTop: 25,
+    marginLeft: 5,
+    marginRight: 5,
+    textAlign: 'center',
+    color: '#fff',
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+});
